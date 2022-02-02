102801547a7a0c3d2dbfc1df6a8b14e634a6a05b
Benjamin <benjamin@Benjamins-MacBook-Pro.local>
2022-02-01 21:20:38 -0600

    b

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,17 @@
+const markdownIt = require("markdown-it");
+const md = new markdownIt({
+  html: true,
+  breaks: true,
+  linkify: true
+});
+
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("assets/images");
     eleventyConfig.addPassthroughCopy("admin");
-
-
+    eleventyConfig.addPassthroughCopy("assets/images");
+    eleventyConfig.addPassthroughCopy("assets/css");
+    eleventyConfig.addWatchTarget("assets/css");
+    eleventyConfig.addPassthroughCopy("assets/scripts");
+    eleventyConfig.addFilter("markdown", (content) => {
+      return md.render(content);
+    });
 }
