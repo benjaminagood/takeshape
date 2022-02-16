57bb2f7adac5fc212cfccc7ac007e9404707022c
Benjamin <benjamin@Benjamins-MacBook-Pro.local>
2022-02-16 12:27:45 -0600

    stylings

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -8,11 +8,21 @@ function header() {
   }
 
 $( document ).ready(function() {
-  $( "li" ).each(function() {
+  $( ".three li" ).each(function() {
     var one = Math.floor(Math.random() * (50 - 30 + 1)) + 30 + '%',
         two = Math.floor(Math.random() * (50 - 30 + 1)) + 30 + '%',
         three = Math.floor(Math.random() * (50 - 30 + 1)) + 30 + '%',
         four = Math.floor(Math.random() * (50 - 30 + 1)) + 30 + '%';
+    $(this).css({"border-top-left-radius": one, "border-top-right-radius": two, "border-bottom-left-radius": three, "border-bottom-right-radius": four});
+  });
+});
+
+$( document ).ready(function() {
+  $( ".three-article hr" ).each(function() {
+    var one = Math.floor(Math.random() * (80 - 30 + 1)) + 30 + '%',
+        two = Math.floor(Math.random() * (80 - 30 + 1)) + 30 + '%',
+        three = Math.floor(Math.random() * (80 - 30 + 1)) + 30 + '%',
+        four = Math.floor(Math.random() * (80 - 30 + 1)) + 30 + '%';
     $(this).css({"border-top-left-radius": one, "border-top-right-radius": two, "border-bottom-left-radius": three, "border-bottom-right-radius": four});
   });
 });
