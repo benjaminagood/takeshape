ddfdae82e7df6382f07c5d3c47965be634ee015e
Benjamin <benjamin@Benjamins-MacBook-Pro.local>
2022-02-16 12:32:03 -0600

    hr style + link style

--- _site/assets/scripts/scripts.js
+++ _site/assets/scripts/scripts.js
@@ -16,13 +16,3 @@ $( document ).ready(function() {
     $(this).css({"border-top-left-radius": one, "border-top-right-radius": two, "border-bottom-left-radius": three, "border-bottom-right-radius": four});
   });
 });
-
-$( document ).ready(function() {
-  $( ".three-article hr" ).each(function() {
-    var one = Math.floor(Math.random() * (80 - 30 + 1)) + 30 + '%',
-        two = Math.floor(Math.random() * (80 - 30 + 1)) + 30 + '%',
-        three = Math.floor(Math.random() * (80 - 30 + 1)) + 30 + '%',
-        four = Math.floor(Math.random() * (80 - 30 + 1)) + 30 + '%';
-    $(this).css({"border-top-left-radius": one, "border-top-right-radius": two, "border-bottom-left-radius": three, "border-bottom-right-radius": four});
-  });
-});
